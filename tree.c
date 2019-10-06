6152ee3fda673e4c29f6983689ecda7d28e4607d
françois de vathaire <fvathaire@linux.home>
2019-10-06 17:09:59 -0400

    add_of_tree.c

---
+++ tree.c
@@ -0,0 +1,21 @@
+
+void tree(int size)
+{
+    int n == size;
+    if (n <= 0) {
+        return;
+    } else {
+        int tree_body(n);
+        int tree_trunk(n);
+        my_putchar(\n);
+    }
+}
+
+void tree_body(n);
+{
+    int t= n +3;
+    while ( t != n) {
+        my puthcar ('*')
+}            
+            
+                    
